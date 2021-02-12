27186a785419dc59a517fc7dc32cb1d1481c5953
Yuli016 <ypantoja@csumb.edu>
2021-02-11 19:07:25 -0800

    Lab 5 Added

---
+++ lab5/js/car.js
@@ -0,0 +1,30 @@
+// car.js
+//Learning how to use Javascript and playing with variables
+// Author: Yulitsa Pantoja <ypantoja@ucsc.edu>
+// Credit: Wes Modes <wmodes@ucsc.edu>
+// Created: February 2021
+// License: Public Domain
+
+console.log("Car")
+
+// Declare variables to hold the make, model, color, and year of your vehicle, example, var year = 2004
+var make = "Honda";
+var model = "Civic";
+var color = "White";
+var year = 2016;
+
+
+// Declare a boolean variable ownIt for whether you own it or not.
+var ownIt = false;
+
+
+// Create a new variable age that subtracts the year from the current year
+var age = 2021 - year;
+
+
+// Use document.writeln() to neatly output each of your variables, example, document.writeln("Make: " + make + "<br>");
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
