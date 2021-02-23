67eb48f9b9d9d20c32ebc21f7a6527e0fdc1d1f1
Yuli016 <ypantoja@csumb.edu>
2021-02-23 13:03:17 -0800

    lab 7 added

---
+++ lab7/js/functions.js
@@ -0,0 +1,105 @@
+// lab.js
+// Learning in Javascript how to use functions
+// Author: Yulitsa Pantoja <ypantoja@ucsc.edu>
+// Source Credit: Wes Modes <wmodes@ucsc.edu>
+// Created: February 2021
+// License: Public Domain
+
+
+function shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+// given a string, return string in Title Case
+// thanks to https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+
+// sortUserName - a function that takes user input and sorts the letters of their name
+    function sortUserName(userName) {
+    //convert to lower case
+    userName = userName.toLowerCase();
+    console.log("userName =", userName);
+
+    //convert to title case
+    userUpper = userName.toUpperCase();
+    console.log("userUpper =", userUpper);
+
+    //split string to array
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+
+    //sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+
+    //join array back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+
+    // // Note that I could have done the above lines as a single line:
+    // //   userName.toLowerCase().split("").sort().join("")
+
+    return userName.toLowerCase().split("").sort().join("");
+}
+
+
+    //sortUserName - a function that takes user input and sorts the letters of their name
+    function randomizeName(userName) {
+
+    // convert userName string to an array
+    var nameArray = userName.toLowerCase().split("");
+    console.log("nameArray =", nameArray);
+
+    // shuffle array with our shuffle function
+    var shuffledArray = shuffleArray(nameArray);
+    console.log("shuffledArray =", shuffledArray);
+    var shuffledString = shuffledArray.join("");
+
+    // shift to Title Case (like a name)
+    var newName = toTitleCase(shuffledString);
+
+    // return array to a string
+    return newName;
+}
+
+
+function main() {
+    var userName = window.prompt("Welcome! Please type your name and see your new name.");
+    document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
+
+    var sortedName = sortUserName(userName);
+    document.writeln("Here's your name sorted in order: " + sortedName + "</br></br>");
+
+    var nameArray = sortUserName(userName);
+    document.writeln("Here's your name sorted in order lower case: " + nameArray.toLowerCase() + "</br></br>");
+
+    var nameUpper = sortUserName(userName);
+    document.writeln("Here's your name sorted in order upper case: " + nameUpper.toUpperCase() + "</br></br>");
+
+    var randomName = randomizeName(userName);
+    document.writeln("Thanks for your input! <br>Here is your new official name make sure you use it from now on.");
+    document.writeln("<div class='name'>" + randomName + "</div>")
+}
+
+main();
